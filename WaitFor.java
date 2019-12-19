34746d41f3bd66ec359e31d74a2dad4022934e2b
shohiebsense <shohiebsense@users.noreply.github.com>
2019-12-19 11:09:51 +0700

    Create WaitFor.java

---
+++ WaitFor.java
@@ -0,0 +1,18 @@
+ public static ViewAction waitFor(final long delay){
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return null;
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
+    }
